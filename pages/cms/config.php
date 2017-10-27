7fdbe36818e27ada0e9391a0a666d9629b3ae52e
youriyep <youriponcia@hotmail.nl>
2017-10-27 10:19:09 +0200

    cms shit

--- pages/cms/config.php
+++ pages/cms/config.php
@@ -3,4 +3,4 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'krashosting');
-$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+$db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
