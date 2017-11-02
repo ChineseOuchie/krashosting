7f3a6051fce7c8a6ef06e8b88322b36e197498b1
youriyep <youriponcia@hotmail.nl>
2017-11-02 16:18:54 +0100

    update.php

--- pages/cms/update.php
+++ pages/cms/update.php
@@ -1,23 +1,25 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
     include_once("config.php");
     $form = $id = '';
     if (isset($_GET) && $_SERVER['REQUEST_METHOD'] === 'GET') {
         $id = $_GET['id'];
         $sql = "SELECT * FROM producten WHERE idproducten = $id;";
-        $result = $db->query($sql);
+        $res = $db->query($sql);
         while ($row = $res->fetch_assoc()) {
-            $form .= "<label for='naam'>Naam: </label>";
-            $form .= "<input id='naam' placeholder='naam' type='text' name='naam' value='" . $row['naam'] . "' required>";
-            $form .= "<label for='ppm'>Prijs per maand: </label>";
-            $form .= "<input id='ppm' placeholder='ppm' type='number' name='ppm' value='" . $row['ppm'] . "'>";
-            $form .= "<label for='mb'>Hoeveelheid MB: </label>";
-            $form .= "<input id='mb' placeholder='mb' type='number' name='mb' value='" . $row['mb'] . "'>";
-            $form .= "<label for='ssl'>Ssl certificaat: (TRUE/FALSE)</label>";
-            $form .= "<input id='ssl' placeholder='ssl' type='text' name='ssl' value='" . $row['ssl'] . "' required>";
-            $form .= "<label for='domeinen'>Aantal domeinen: </label>";
-            $form .= "<input id='domeinen' placeholder='domeinen' type='number' name='domeinen' value='" . $row['domeinen'] . "' required>";
-            $form .= "<label for='bandbreedte'>Hoeveelheid bandbreedte: </label>";
-            $form .= "<input id='bandbreedte' placeholder='bandbreedte' type='text' name='bandbreedte' value='" . $row['bandbreedte'] . "'>";
+            $form .= "<label for='naam'>Naam: </label><br>";
+            $form .= "<input id='naam' placeholder='naam' type='text' name='naam' value='" . $row['naam'] . "' required><br>";
+            $form .= "<label for='ppm'>Prijs per maand: </label><br>";
+            $form .= "<input id='ppm' placeholder='ppm' type='number' name='ppm' value='" . $row['ppm'] . "' required><br>";
+            $form .= "<label for='mb'>Hoeveelheid MB: </label><br>";
+            $form .= "<input id='mb' placeholder='mb' type='number' name='mb' value='" . $row['mb'] . "' required><br>";
+            $form .= "<label for='ssl'>Ssl certificaat: (TRUE/FALSE)</label><br>";
+            $form .= "<input id='ssl' placeholder='ssl' type='text' name='ssl' value='" . $row['ssl'] . "' required><br>";
+            $form .= "<label for='domeinen'>Aantal domeinen: </label><br>";
+            $form .= "<input id='domeinen' placeholder='domeinen' type='number' name='domeinen' value='" . $row['domeinen'] . "' required><br>";
+            $form .= "<label for='bandbreedte'>Hoeveelheid bandbreedte: </label><br>";
+            $form .= "<input id='bandbreedte' placeholder='bandbreedte' type='text' name='bandbreedte' value='" . $row['bandbreedte'] . "' required><br>";
         }
         $db->close();
     }
