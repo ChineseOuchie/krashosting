61b2560a98e28a69788489a85ba4a1c40e94a6e0
Joey <joeylau1998@gmail.com>
2017-11-10 11:23:26 +0100

    New Website design

---
+++ Oud website/index.php
@@ -0,0 +1,74 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$out = '';
+$outNieuws = "";
+$conn = new mysqli('localhost', 'root', 'root', 'krashosting');
+$query = 'SELECT * FROM producten';
+$res = $conn->query($query);
+while($row = $res->fetch_assoc()){
+    $gb = $row['mb'] / 1000;
+    $ssl = $row['ssl'];
+	if($ssl === 'TRUE'){
+		$ssl = 'Yes';
+	}else{
+		$ssl = 'No';
+	}
+
+    $out.= '<div class="columns">';
+    $out.= '<ul class="price">';
+    $out.= '<li class="header">' . ucfirst($row['naam']) . '</li>';
+    $out.= '<li class="grey">€' . $row['ppm'] . ' / month</li>';
+    $out.= '<li>' . $gb . 'GB Storage</li>';
+    $out.= '<li>SSL Certificate: ' . $ssl . '</li>';
+    $out.= '<li>' . $row['domeinen'] . ' Domains</li>';
+    $out.= '<li>' . ucfirst($row['bandbreedte']) . ' Bandwidth</li>';
+    $out.= '<li class="grey"><a href="#" class="button">Sign Up</a></li>';
+    $out.= '</ul>';
+    $out.= '</div>';
+}
+$queryNieuws = 'SELECT * FROM nieuws';
+$resNieuws = $conn->query($queryNieuws);
+while($rowNieuws = $resNieuws->fetch_assoc()) {
+    $outNieuws .= "<div class='item'>{$rowNieuws['titel']}<br>{$rowNieuws['bericht']}</div>";
+}
+
+$conn->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kras Hosting</title>
+    <link href="Oud website/css/main.css" rel="stylesheet" type="text/css">
+    <script src="Oud website/js/main.js" defer></script>
+</head>
+    <body>
+        <div class="container">
+            <div class="banner">
+                <img class="banner" src="Oud website/img/banner.gif">
+            </div>
+            <nav id="nav">
+                <a class="link active" href="index.php">Home</a>
+                <a class="link" href="pages/pakketten.php">Package</a>
+                <a class="link" href="pages/contact.php">Contact</a>
+                <a class="link" href="pages/about.php">About Us</a>
+            </nav>
+            <article id="artcl1">
+                <?php echo $out?>
+            </article>
+            <div class="pro"><a href="pages/custom.php">Pro/Custom</a></div>
+            <div class="slider">
+                <div class="nieuws slide">
+<!--                    <div class="item">Nieuws Bericht #1</div>-->
+<!--                    <div class="item">Nieuws Bericht #2</div>-->
+<!--                    <div class="item">Nieuws Bericht #3</div>-->
+<!--                    <div class="item">Nieuws Bericht #4</div>-->
+<!--                    <div class="item">Nieuws Bericht #5</div>-->
+                    <?php echo $outNieuws;?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
